2950153734d7814189901ddd0e7a272f7f17282a
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-09-04 02:02:42 +0530

    Updated Nested Inner Class

--- src/nested_innerclass.java
+++ src/nested_innerclass.java
@@ -15,43 +15,19 @@ class NestedOuter{
     void display(){
         System.out.println("Calling outerclass");
     }
-    public class NestedInner{
+    protected class NestedInner{
         int inner = 100;
-        // outer =60; -> Inner class can just access outer class variables and methods but they cant modify it.
-        // display(); -> can't access
-        void screen(){
+        void display(){
             System.out.println("Calling innerclass");
-            System.out.println(outer);
-            display();
-        }
-        void access_outerproperties(){
-            NestedOuter nn = new NestedOuter();
-            System.out.println(nn.outer);
-            nn.display();
         }
     }
-
-    void access_innerproperties(){
-        //System.out.println(inner);
-        //screen();
-        NestedOuter.NestedInner n = new NestedOuter.NestedInner();
-        System.out.println(n.inner);
-        n.screen();
     }
-}
 public class nested_innerclass {
     public static void main(String ...args){
         NestedOuter.NestedInner nsi= new NestedOuter().new NestedInner();
-        System.out.println(nsi.inner);
-        nsi.screen();
-        nsi.access_outerproperties();
+        nsi.display();
 
         NestedOuter ns = new NestedOuter();
-        System.out.println("____________");
-        System.out.println(ns.outer);
         ns.display();
-        ns.access_innerproperties();
-
     }
-
 }
