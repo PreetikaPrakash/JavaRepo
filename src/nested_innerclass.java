9752e343c3e254b2129187f6b8b099c1a084ec07
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-09-04 02:03:38 +0530

    Updated Nested Inner Class

--- src/nested_innerclass.java
+++ src/nested_innerclass.java
@@ -1,15 +1,4 @@
-/* OVERVIEW -
-    - A Class inside a class, but in a hierarchy.
-    - outer class holds inner class
 
-    - How can inner class access outer class variables and methods ?
-        ->
-    - How can outer class " ?
-    - can outer and inner override ?
-    - Can/How main function call outer/inner ?
-    - is strict/non-strict hierarchy applicable here ?
-
-*/
 class NestedOuter{
     int outer = 50;
     void display(){
