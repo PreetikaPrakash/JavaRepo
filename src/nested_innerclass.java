f8447844fa093ad45fee74b690694861a6068795
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-09-04 01:45:52 +0530

    Updated Nested Inner Class

--- src/nested_innerclass.java
+++ src/nested_innerclass.java
@@ -1,7 +1,9 @@
 /* OVERVIEW -
     - A Class inside a class, but in a hierarchy.
     - outer class holds inner class
+
     - How can inner class access outer class variables and methods ?
+        ->
     - How can outer class " ?
     - can outer and inner override ?
     - Can/How main function call outer/inner ?
@@ -9,20 +11,47 @@
 
 */
 class NestedOuter{
-    final int outer = 50;
-
-    public class NestedInner{
-        int inner = 100;
-        void screen(){
-            System.out.println("Calling innerclass");
-            System.out.println(outer);
-        }
-        //outer = 40;
-    }
+    int outer = 50;
     void display(){
         System.out.println("Calling outerclass");
     }
+    public class NestedInner{
+        int inner = 100;
+        // outer =60; -> Inner class can just access outer class variables and methods but they cant modify it.
+        // display(); -> can't access
+        void screen(){
+            System.out.println("Calling innerclass");
+            System.out.println(outer);
+            display();
+        }
+        void access_outerproperties(){
+            NestedOuter nn = new NestedOuter();
+            System.out.println(nn.outer);
+            nn.display();
+        }
+    }
+
+    void access_innerproperties(){
+        //System.out.println(inner);
+        //screen();
+        NestedOuter.NestedInner n = new NestedOuter.NestedInner();
+        System.out.println(n.inner);
+        n.screen();
+    }
 }
 public class nested_innerclass {
+    public static void main(String ...args){
+        NestedOuter.NestedInner nsi= new NestedOuter().new NestedInner();
+        System.out.println(nsi.inner);
+        nsi.screen();
+        nsi.access_outerproperties();
+
+        NestedOuter ns = new NestedOuter();
+        System.out.println("____________");
+        System.out.println(ns.outer);
+        ns.display();
+        ns.access_innerproperties();
+
+    }
 
 }
