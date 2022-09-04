c84a9bc439081a1ca4b5083f3c6020b973988af6
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-09-04 21:33:13 +0530

    Static Blocks

--- src/static_block.java
+++ src/static_block.java
@@ -1,15 +1,18 @@
-
-public class static_block {
-
-    public static void main(String...args){
-        System.out.println("Main Method");
-    }
+class static_blocks{
     static{
         System.out.println("Static block 1");
     }
     static{
         System.out.println("Static block 2");
     }
+}
+public class static_block {
+
+    public static void main(String...args){
+        static_blocks sb = new static_blocks();
+        System.out.println("Main Method");
+    }
+
 }
 
 
