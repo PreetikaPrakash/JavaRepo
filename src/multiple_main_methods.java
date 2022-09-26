1551444f3562348f00235635b94f5c7edde36d59
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-09-26 22:44:17 +0530

    Multiple main methods

---
+++ src/multiple_main_methods.java
@@ -0,0 +1,16 @@
+
+class multiple_main_methods_two {
+    public static void main(String[] args){
+        System.out.println("Class second");
+
+    }
+}
+class multiple_main_methods_one {
+    public static void main(String[] args){
+        System.out.println("Class first");
+        int x=5;
+        System.out.println(++x);
+        int y=5;
+        System.out.println(y++);
+    }
+}
