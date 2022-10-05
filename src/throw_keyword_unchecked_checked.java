4babdc554b571f2f7fb9c5cf97120e603dc7d035
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-10-06 01:16:10 +0530

    throw and throws

---
+++ src/throw_keyword_unchecked_checked.java
@@ -0,0 +1,34 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class throw_keyword_unchecked_checked {
+    // unchecked
+    static void func1(int age){
+        if(age<18){
+            // Arithmetic exception is unchecked exception so it is not mandatory to include try catch
+            throw new ArithmeticException("Not eligible to vote");
+        }
+        else
+        {
+            System.out.println("Eligible to vote");
+        }
+    }
+
+    //checked
+    static void func2() throws FileNotFoundException {
+        //IOException
+        FileInputStream fo = new FileInputStream("src/file.txt");
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age and check are you eligible to vote?");
+        int age = sc.nextInt();
+        func1(age);
+        try {
+            func2();
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        }
+    }
+}
--- src/userdefined_exception.java
+++ src/userdefined_exception.java
@@ -1,21 +1,22 @@
-
-public class userdefined_exception {}
-    /*class Userdefined extends Exception{
-        public String toString(){
-            return "this is my exception message";
-        }
-    }  public static void main(String args[]){
-        myclass cl = new myclass();
-        cl.func1(10);
+class Userdefined extends Exception{
+    public String toString(){
+        return "this is my exception message";
     }
-    }
-/*class myclass {
+}
+public class userdefined_exception {
     static void func1(int a) {
         try {
-            System.out.println(4 / 0);
             throw new Userdefined();
         } catch (Userdefined e) {
             System.out.println(e);
         }
     }
-}*/
+    public static void main(String args[]){
+        func1(10);
+    }
+}
+
+
+
+
+
