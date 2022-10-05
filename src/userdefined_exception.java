9ade0caf6ae7b66264432b2a518519ad0bedca89
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-10-06 03:59:13 +0530

    user defined exception

--- src/userdefined_exception.java
+++ src/userdefined_exception.java
@@ -11,8 +11,24 @@ public class userdefined_exception {
             System.out.println(e);
         }
     }
+    // this method will not handle the exception instead it will pass on the handling to calling method (psvm)
+    static void func2(int a) throws Userdefined {
+        if(a<20){
+            throw new Userdefined();
+        }
+        else{
+        System.out.println("Inside func2");}
+    }
     public static void main(String args[]){
         func1(10);
+        try {
+            // if a = 20, then Inside func2 is printed otherwise if a = 10 then exception is catched below
+            func2(10);
+        }
+        catch (Userdefined userdefined) {
+
+            System.out.println(userdefined);
+        }
     }
 }
 
