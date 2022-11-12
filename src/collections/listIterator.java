7e1946eb305d5d213c9e3bdf552b41dfdcf9f42d
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-11-12 21:33:01 +0530

    listIterator.java

---
+++ src/collections/listIterator.java
@@ -0,0 +1,30 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class listIterator {
+
+    public static void main(String[] args) {
+            List<String> ls = new ArrayList<>();
+            ls.add("yellow");
+            ls.add("green");
+            ls.add("blue");
+            ls.add("pink");
+            ls.add("red");
+
+            // FORWARD PRINTING
+            ListIterator li = ls.listIterator();
+            while(li.hasNext()){
+                System.out.print(li.nextIndex()+" "+li.next()+" ");
+            }
+
+        System.out.println();
+
+            // BACKWARD PRINTING
+            while(li.hasPrevious()){
+                System.out.print(li.previousIndex()+" "+li.previous()+" ");
+            }
+    }
+}
