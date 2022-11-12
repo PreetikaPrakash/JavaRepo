54051c4b3f1a7841db655eaa8ef01f592553a568
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-11-12 21:32:50 +0530

    linkedListUserDefinedBookClass.java

---
+++ src/collections/linkedListUserDefinedBookClass.java
@@ -0,0 +1,32 @@
+package collections;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class Book{
+    int id;
+    String book_name;
+    String book_author;
+
+    Book(int id, String book_name, String book_author){
+        this.id = id;
+        this.book_name = book_name;
+        this.book_author = book_author;
+    }
+
+}
+public class linkedListUserDefinedBookClass {
+
+    public static void main(String[] args) {
+        LinkedList<Book> ls = new LinkedList<>();
+        Book b1 = new Book(101,"The Notebook","Nicholas Sparks");
+        Book b2 = new Book(102, "The Great Gatsby","F. Scott Fitzgerald.");
+        ls.add(b1);
+        ls.add(b2);
+
+        for(Book b: ls){
+            System.out.println(b.id+" -|- "+b.book_author+" -|- "+b.book_name);
+        }
+
+    }
+}
