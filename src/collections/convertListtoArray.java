fcd7199001c5d147639ca5b65d3d485f1a019932
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-11-12 21:32:23 +0530

    convertListtoArray.java

---
+++ src/collections/convertListtoArray.java
@@ -0,0 +1,23 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class convertListtoArray {
+    public static void main(String[] args) {
+        List<String> ls = new LinkedList<>();
+        ls.add("Pen");
+        ls.add("Paint Brush");
+        ls.add("Sharpener");
+        ls.add("Eraser");
+        ls.add("Box");
+
+
+        String arr[] = ls.toArray(new String[ls.size()]);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
